da2e54f9a5e9c6536cbfed0a9dd08ae9e98d9be7
Peter Magdy <petermagdyiskander@gmail.com>
2025-04-20 04:06:18 +0200

    add storage backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
+import { uploadData } from 'aws-amplify/storage';
 
 Amplify.configure(outputs);
 
@@ -15,4 +16,25 @@ Amplify.configure(outputs);
 })
 export class AppComponent {
   title = 'amplify-angular-template';
+  selectedFile: File | null = null;
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+    }
+  }
+  handleClick() {
+    if (this.selectedFile) {
+      console.log(
+        uploadData({
+          data: this.selectedFile,
+          path: `picture-submissions/${this.selectedFile.name}`,
+          options: {
+            // Specify a target bucket using name assigned in Amplify Backend
+            bucket: 'amplifyTeamDrive',
+          },
+        })
+      );
+    }
+  }
 }
