049e286593fbf8c4f62fe48c330511afdbcfb0ee
Peter Magdy <petermagdyiskander@gmail.com>
2025-04-22 21:13:09 +0200

    [ADD] added new model

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -12,6 +12,17 @@ const schema = a.schema({
       content: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
+  Video: a.model({
+    // ID field is automatically added by Amplify as the primary key
+
+    // Video metadata fields
+    title: a.string().required(),
+    description: a.string(),
+    ownerName: a.string(), // Username of owner
+    s3Key: a.string().required(), // S3 key for the video file
+    createdAt: a.datetime().required().default('now'),
+    updatedAt: a.datetime().required().default('now'),
+  }),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -32,7 +43,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,10 @@ import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
 import { uploadData } from 'aws-amplify/storage';
+import { generateClient } from 'aws-amplify/data';
+import type { Schema } from '../../amplify/data/resource';
 
+const client = generateClient<Schema>();
 Amplify.configure(outputs);
 
 @Component({
@@ -25,9 +28,10 @@ export class AppComponent {
   }
   async handleClick() {
     if (this.selectedFile) {
+      const s3Key = this.selectedFile.name;
       try {
         const result = await uploadData({
-          path: `picture-submissions/${this.selectedFile.name}`,
+          path: `picture-submissions/${s3Key}`,
           // Alternatively, path: ({identityId}) => `album/${identityId}/1.jpg`
           data: this.selectedFile,
           options: {
@@ -42,9 +46,17 @@ export class AppComponent {
             },
           },
         }).result;
-        console.log("Path from Response: ", result.path);
+        client.models.Video.create({
+          title: 'Video 1',
+          description: 'Video description',
+          ownerName: 'Peter ISKANDER',
+          s3Key: s3Key,
+          createdAt: new Date().toISOString(),
+          updatedAt: new Date().toISOString(),
+        });
+        console.log('Path from Response: ', result.path);
       } catch (error) {
-        console.log("Error : ", error);
+        console.log('Error : ', error);
       }
     }
   }
